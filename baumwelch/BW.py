93d639a222c75ea1793846e8b020ce635a8f4c06
adrienbrunet <abrunet@centrale-marseille.fr>
2012-12-17 22:24:54 +0100

    sphinx et debut de documentation

--- baumwelch/BW.py
+++ baumwelch/BW.py
@@ -15,8 +15,10 @@ import apprentissage as app
 
 
 class HMM_BW(object):
-
+	'''Definition d'une classe et donc d'un HMM comme étant un objet, calcul et etiration via l'algorithme de Baum Welch'''
+	
 	def __init__(self, listObservables, count):
+		'''initialisation du hmm'''
 		self.iterationCount = count
 		self.listObservables = listObservables
 		self.initHmm(listObservables)
@@ -38,6 +40,7 @@ class HMM_BW(object):
 
 
 	def initHmm(self, listObservables):
+		'''Initialisation du hmm lambda_0 et premier calcul pour les probabilites de transitions, emissions et initiales (equiprobabilité partout)'''
 		self.states = encod.get_categories(data+"/voc_etats")
 
 		self.Pi = {}
@@ -59,6 +62,8 @@ class HMM_BW(object):
 		return [self.states, self.transitions, self.emissions]
 
 	def setAlpha(self):
+		'''Calcul des alpha(i,t) tels que definis dans les slides,  correspond à la probabilité de se trouver dans
+l’etat i du HMM lambda a un instant t, ayant observe la suite o1 . . . ot−1 '''
 		# init alpha(i, 1) = Pi(i) #
 		for state in self.Pi:
 			self.alpha[0][state] = self.Pi[state]
@@ -74,6 +79,7 @@ class HMM_BW(object):
 		
 
 	def setBeta(self):
+		'''Calcul des beta(i,t) tels que definis dans les slides, meme calcul que pour les alpha mais avec un parcours inverse du treillis.'''
 		# init beta(i, T) = E(i, o_t) #
 		for state in self.states:
 			self.beta[len(self.listObservables)-1][state] = self.emissions[self.listObservables[len(self.listObservables)-1]][state]
@@ -87,9 +93,12 @@ class HMM_BW(object):
 				self.beta[index][j] = sumBeta
 
 	def setGamma(self):
+		'''calcul des gammas(i,t), probabilite que lambda_0 soit dans l'etat i a l'instant t'''
 		a = 1+1
 		#for element in grid:
 		#	do sth
+
+		
 
 	def setPi(self):
 		a = 1+1
--- index.rst
+++ index.rst
@@ -1,15 +1,62 @@
-.. Baum Welch documentation master file, created by
-   sphinx-quickstart on Thu Dec 13 16:48:34 2012.
+.. BaumWelch documentation master file, created by
+   sphinx-quickstart on Mon Dec 17 17:58:37 2012.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to Baum Welch's documentation!
-======================================
+Welcome to BaumWelch's documentation!
+=====================================
 
 Contents:
 
 .. toctree::
    :maxdepth: 2
+
+
+Baum Welch 
+=========================
+
+Utilisation de l'algorithme de Baum Welch, implémentation, initialisation et itération.
+
+BW.py
++++++++++++++++++++++++++
+
+.. automodule:: BW
+    :members:
+
+HMM et Viterbi
+==========================
+
+Utilisation de l'algorithme de Viterbi. Construction du HMM.
+
+apprentissage.py
++++++++++++++++++++
+
+.. automodule:: apprentissage
+	:members:
+
+encodageHmm.py
++++++++++++++++++++
+
+
+.. automodule:: encodageHmm
+	:members:
+
+viterbi.py
+++++++++++++++++++
+
+.. automodule:: viterbi
+	:members:
+
+Tests
+==========================
+
+Dans un souci de développement "propre", i.e. par les tests, nous avons tenu à mettre en place des tests unitaires pour guider le développement de nos algorithmes.
+
+test_BW.py
+++++++++++++++++++++++
+
+.. automodule:: test_BW
+	:members:
 
 
 
